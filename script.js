2f003777693d2ba4565af1d2bd4a08cabc130c43
PritamSinhaa <pritamsinha2323548@gmail.com>
2025-12-16 20:22:00 +0530

    js

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 var swiper = new Swiper(".mySwiper", {
       loop:true,
       navigation: {
-        nextEl: ".fa-arrow-left",
-        prevEl: ".fa-arrow-right",
+        nextEl: ".arrow-left",
+        prevEl: ".arrow-right",
       },
     });
+
+ const cartIcon = document.querySelector(".cart-icon") 
+ const cartTab = document.querySelector(".cart-tab") 
+ const closeButton = document.querySelector(".close-btn") 
+
+ const openCart = () => cartTab.classList.toggle("cart-tab-active")
+ 
+
+ cartIcon.addEventListener("click",openCart)
+ closeButton.addEventListener("click",openCart)
+
+ let productList = [];
+
+ const initApp = () =>{
+
+  fetch("products.json").then
+  (response => response.json()).then
+  (data =>{
+
+    productList = data;
+    console.log(productList)
+  })
+
+ }
+
+ initApp();
